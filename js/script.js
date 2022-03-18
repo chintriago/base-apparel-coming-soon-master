e33d90e3ec01f0f0299eda0f4737a2e6425754d7
Chris Intriago <chrisintriago94@gmail.com>
2022-03-18 13:17:20 -0700

    cleaned up code

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
-$("#emailForm").submit(function(event) {
+$("#emailForm").submit(function (event) {
     event.preventDefault();
     let email = $("#mail").val();
     let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(email.match(mailFormat)) {
+    if (email.match(mailFormat)) {
         $("#mail").css("border", "1px solid rgb(216, 208, 208)");
         $("#error-paragraph").css("visibility", "hidden");
-        $("#error-arrow").css("display", "none")
-      console.log(email);
+        $("#error-arrow").css("display", "none");
+        console.log(email);
     } else {
         $("#mail").css("border", "1px solid red");
         $("#error-paragraph").css("visibility", "visible");
-        $("#error-arrow").css("display", "inline-block")
+        $("#error-arrow").css("display", "inline-block");
         console.log("invalid email");
     }
 });
-
